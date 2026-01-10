3cde5c1d4386687aae32f8b9182f7e030d1288b9
Prasenjit Sharma <prasenjitnitdgp@gmail.com>
2026-01-10 17:21:48 +0530

    minor

--- pages/4_cust_avg_price.py
+++ pages/4_cust_avg_price.py
@@ -11,14 +11,11 @@ import utilities
 # -------------------------------------------------
 utilities.apply_common_styles("Material Pricing")
 # In initial logged_in is False
-if "is_logged_in" not in st.session_state:
-    st.session_state["is_logged_in"] = False
-    
-if st.session_state["is_logged_in"]:
-    df = st.session_state["Sales Data"]
-    discount_json = st.session_state["Discount Data"]
 
-    df = utilities.prepare_df_for_aggrid(df, columns_to_convert=["Fiscal Year"])
+df = st.session_state["Sales Data"]
+discount_json = st.session_state["Discount Data"]
+
+df = utilities.prepare_df_for_aggrid(df, columns_to_convert=["Fiscal Year"])
 
 def prepare_material_price_metrics(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
